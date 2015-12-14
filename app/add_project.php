2ed13a7a38de870faadc567efebe7aa29d5ac096
kryshtopa <followthewhite@yandex.ru>
2015-12-14 13:00:23 +0100

    Установлены необходимые плагины

---
+++ app/add_project.php
@@ -0,0 +1,10 @@
+<?php
+
+  $data = array();
+  $data['mes'] = 'OK';
+
+  header("Content-Type: application/json");
+  echo json_encode($data);
+  exit;
+
+?>
--- app/js/popup.js
+++ app/js/popup.js
@@ -1,37 +1,54 @@
-var myModule = (function () {
-
-  var initIn = function () {
-    _setUpListners();
-  };
-
-  var _setUpListners = function () {
-    $('.work-new').on('click', _showPopup); // включаем всплывающее окно
-    $('.work-add').on('click', _addWork); // добавляем проект
-  };
-
-  var _showPopup = function (event) {
-    event.preventDefault();
-
-    $('.popup-work').bPopup({
-      //поведение всплывающего окна
-
-    });
-  };
-
-  var _addWork = function () {
-    var form = $(this);
-    console.log(form);
-    $.ajax ({
-
-    });
-
-    return false; // или function(event) -> event.preventDefault();
-  }
-
-  return {
-    init : initIn
-  };
-
-})();
-
-myModule.init();
+// var myModule = (function () {
+//
+//   var initIn = function () {
+//     _setUpListners();
+//   };
+//
+//   var _setUpListners = function () {
+//     $('.work-new').on('click', _showPopup); // включаем всплывающее окно
+//     $('.work-add').on('click', _addWork); // добавляем проект
+//   };
+//
+//   var _showPopup = function (event) {
+//     event.preventDefault();
+//
+//     $('.popup-work').bPopup({
+//       //поведение всплывающего окна
+//
+//     });
+//   };
+//
+//   var _addWork = function (event) {
+//     event.preventDefault();
+//
+//
+//     var form = $(this),
+//         url = 'add_project.php',
+//         data =  form.serialize();
+//
+//     //запрос на сервер
+//     $.ajax({
+//       url: url,
+//       type: 'POST',
+//       dataType: 'json',
+//       data: data,
+//     })
+//     .done(function(ans) {
+//       form.find('.popup-alert-error').text();
+//     })
+//     .fail(function() {
+//       console.log("error");
+//     })
+//     .always(function() {
+//       console.log("complete");
+//     });
+//
+//   }
+//
+//   return {
+//     init : initIn
+//   };
+//
+// })();
+//
+// myModule.init();
